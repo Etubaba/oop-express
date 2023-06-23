b9fee6335876fc7600a2e00a2c72503aa7538638
dom000 <godknowsegiboy00@gmail.com>
2023-06-23 18:46:27 +0100

    quick fix

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 // import { HttpException } from '@exceptions/httpException';
-import { logger } from '../utils/logger';
+// import { logger } from '../utils/logger';
 import { HttpException } from '@/utils/HttpException';
 
 export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +8,7 @@ export const ErrorMiddleware = (error: HttpException, req: Request, res: Respons
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+    // logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
     res.status(status).json({ message });
   } catch (error) {
     next(error);
