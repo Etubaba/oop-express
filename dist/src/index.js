5af629363031a681b754383809f9292b0eb785d1
dom000 <godknowsegiboy00@gmail.com>
2023-06-25 23:02:27 +0100

    update

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const routes_1 = require("./modules/Auth/routes");
 const app_1 = require("./app");
 const validateEnv_1 = require("./utils/validateEnv");
-const routes_2 = tslib_1.__importDefault(require("./modules/Admin/routes"));
+// import AdminRoute from './modules/Admin/routes';
 (0, validateEnv_1.ValidateEnv)();
-const app = new app_1.App([new routes_1.AuthRoute(), new routes_2.default()]);
+const app = new app_1.App([new routes_1.AuthRoute()]);
 app.listen();
 //# sourceMappingURL=index.js.map
